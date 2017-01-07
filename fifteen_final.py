102ee4c767b90425efc6c6e090699fb6b00b6fdb
Ramadan K.Mostafa <ramadankhalifamostafa@gmail.com>
2017-01-07 21:07:24 +0200

    Update fifteen_final.py

--- fifteen_final.py
+++ fifteen_final.py
@@ -2,6 +2,11 @@
 Loyd's Fifteen puzzle - solver and visualizer
 Note that solved configuration has the blank (zero) tile in upper left
 Use the arrows key to swap this tile with its neighbors
+
+build a solver for the puzzle. the solution process for a puzzle of size m×n has three phases:
+-Solve the bottom m−2 rows of the puzzle in a row by row manner from bottom to top. Each individual row will be solved in a right to left order.
+-Solve the rightmost n−2 columns of the top two rows of the puzzle (in a right to left order). Each column consists of two unsolved positions and will be solved in a bottom to top order.
+-Solve the upper left 2×2 portion of the puzzle directly.
 """
 
 import poc_fifteen_gui
